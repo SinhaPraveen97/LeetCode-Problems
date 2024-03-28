46be57d377a5f26c8e19bfe8176060d59610cd87
Praveen Sinha <38722697+SinhaPraveen97@users.noreply.github.com>
2024-03-28 22:42:57 +0530

    Time: 147 ms (21.87%) | Memory: 47.2 MB (94.67%) - LeetSync

---
+++ 1-two-sum/two-sum.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+       bool flag = false;
+            int[] result = new int[2];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (flag)
+                    break;
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                    {
+                        result[0] = i;
+                        result[1] = j;
+                        flag = true;
+                        break;
+                    }
+                }
+            }
+            return result;
+    }
+}
